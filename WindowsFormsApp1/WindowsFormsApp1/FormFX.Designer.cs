03ad74a3f07780f19cb1adabe2fd4c95d84328e8
allen98787 <allen98787@gmail.com>
2023-06-14 13:26:33 +0800

    6/14 over

--- WindowsFormsApp1/WindowsFormsApp1/FormFX.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FormFX.Designer.cs
@@ -29,44 +29,57 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ShowOrder = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(98, 73);
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.Size = new System.Drawing.Size(122, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Order List";
             // 
-            // button1
+            // ShowOrder
             // 
-            this.button1.Location = new System.Drawing.Point(101, 255);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 48);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "讀取菜單";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ShowOrder.Location = new System.Drawing.Point(17, 211);
+            this.ShowOrder.Name = "ShowOrder";
+            this.ShowOrder.Size = new System.Drawing.Size(152, 48);
+            this.ShowOrder.TabIndex = 1;
+            this.ShowOrder.Text = "讀取菜單";
+            this.ShowOrder.UseVisualStyleBackColor = true;
+            this.ShowOrder.Click += new System.EventHandler(this.ShowOrder_Click);
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(101, 91);
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 41);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(408, 124);
+            this.checkedListBox1.Size = new System.Drawing.Size(589, 164);
             this.checkedListBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "刪除點單";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DeleteBT_Click);
             // 
             // FormFX
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkedListBox1);
+            this.ClientSize = new System.Drawing.Size(627, 315);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.ShowOrder);
             this.Controls.Add(this.label1);
             this.Name = "FormFX";
             this.Text = "FormFX";
@@ -78,7 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ShowOrder;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
